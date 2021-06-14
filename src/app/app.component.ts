1fcd8dc7f238566ac4b31fc505573de601b08b70
Bogdan Moskvitin <moskvitin.bg@gmail.com>
2021-06-14 12:02:50 +0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
 import { Component } from '@angular/core';
 
+import { FormGroup, FormControl, Validators} from '@angular/forms';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'card';
+
+  month: string = "";
+  year: string = "";
+  months = ["01","02","03","04","05","06","07","08","09","10","11","12"];
+  years = ["2021","2022","2023","2024","2025","2026","2027","2028","2029","2030","2031","2032"];
+
+  visibility: boolean = false;
+
+  toggle(){
+    this.visibility = !this.visibility;
+  }
+
+  myForm: FormGroup;
+  constructor(){
+    this.myForm = new FormGroup({
+      "userNumber": new FormControl("#### #### #### ####", [Validators.required, Validators.pattern("[0-9 A-Z]{16}")]),
+      "userName": new FormControl("FULL NAME", [Validators.required, Validators.pattern("[A-Z]*")]),
+      "userCvv": new FormControl("", [Validators.required, Validators.pattern("[0-9]{3}")])
+    }) 
+  }
 }
